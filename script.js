0f786ba296efa3911a5aab09d96ebc2d23bb718d
julianll1202 <a20490712@itmexicali.edu.mx>
2022-08-22 20:58:55 -0700

    Created variables, obtained subtotal

---
+++ script.js
@@ -0,0 +1,7 @@
+let productName = prompt("Enter product name:");
+let productPrice = prompt("Enter product pricce:");
+let productQty = prompt("Enter product quantity:");
+let subTotal = productPrice*productQty;
+console.log(productName);
+console.log(subTotal);
+console.log(subTotal*.8);

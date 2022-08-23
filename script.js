3e259a5498cbfb1920748a24491cb5460b280e9e
julianll1202 <a20490712@itmexicali.edu.mx>
2022-08-23 09:51:49 -0700

    Output of stored information to console

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let productName = prompt("Enter product name:");
 let productPrice = prompt("Enter product pricce:");
 let productQty = prompt("Enter product quantity:");
 let subTotal = productPrice*productQty;
-console.log(productName);
-console.log(subTotal);
-console.log(subTotal*.8);
+let salesTax = subTotal*0.0725;
+let total = salesTax+subTotal;
+
+console.log("\tWlecome to Best Buy");
+console.log("                   ");
+console.log(productQty,"   ",productName);
+console.log("                ---------");
+console.log("Subtotal       ",subTotal);
+console.log("Sales Tax      ",salesTax);
+console.log("                =========");
+console.log("Total          ",total);
+
